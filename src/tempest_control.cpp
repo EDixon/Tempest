dfc36595c5965fb2a0d52e3636cad44b7fabbd62
EDixon <eliotdixon@gmx.co.uk>
2017-02-03 15:27:43 +0000

    started writing the command system

--- src/tempest_control.cpp
+++ src/tempest_control.cpp
@@ -9,12 +9,64 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <std_msgs/String.h>
+#include <string>
+#include <boost/algorithm/string.hpp>
+
+enum states {PREPARE,LAUNCH,LOITER,WAYPOINT,WAYPOINT_DONE,RTL,WAIT_LAND,LAND,HARD_LAND,COM_CHECK,SHUTDOWN};
+enum commands {START, STOP, WAYPOINT, CANCEL, PING, LAND}
+
+
+class command{
+    public:
+        commands state;
+        double lat;
+        double lng;
+        double alt;
+        double speed;
+        double heading;
+        int waypoint_n
+        command();
+};
+
+command::command(void){
+    lat = 0.0;
+    lng = 0.0;
+    alt = 0.0;
+    speed = 0.0;
+    heading = 0.0;
+    waypoint_n = 0;
+}
 
 mavros_msgs::State current_state;
-enum states {PREPARE,LAUNCH,LOITER,WAYPOINT,WAYPOINT_DONE,RTL,WAIT_LAND,LAND,HARD_LAND,COM_CHECK,SHUTDOWN};
-
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
+void state_cb(const mavros_msgs::State::ConstPtr &msg){
     current_state = *msg;
+}
+
+std:vector<std::string> split(const std::string &line){
+    std::vector<std::string> parts;
+    boost::split(parts)
+}
+
+command current_command;
+void command_cb(const std_msgs::String::ConstPtr &msg){
+    std::string incoming = msg->data;
+    if (incoming.compare(0,7,"$START,") == 0){
+        current_command.state = START;
+    } else if (incoming.compare(0,6,"$STOP,") == 0){
+        current_command.state = STOP;
+    } else if (incoming.compare(0,10,"$WAYPOINT,") == 0){
+        std::vector<std::string> parts;
+        
+        
+        current_command.state = WAYPOINT;
+    } else if (incoming.compare(0,8,"$CANCEL,") == 0){
+        current_command.state = CANCEL;
+    } else if (incoming.compare(0,6,"$PING,") == 0){
+        current_command.state = PING;
+    } else if (incoming.compare(0,10,"$LAND,") == 0){
+        current_command.state = LAND;
+    }
 }
 
 int main(int argc, char **argv)
@@ -31,41 +83,52 @@ int main(int argc, char **argv)
             ("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
+    ros:Subscriber command_sub = nh.subscribe<std_msgs::String>("to_front_seat",1000,command_cb)
     
     ros::Rate rate(20.0);
 
     //main control loop, holds the state machine
     states current = PREPARE;
-    bool connected = false;
+    bool armed = false;
     ros::Time last_request = ros::Time::now();
     while(ros::ok()){
         switch(current) {
+            //check that the pixhawk is still connected
             case COM_CHECK:
-
+                
                 break;
+            //connect to the pixhawk and arm
             case PREPARE:
-                if (connected == false){
-                    mavros_msgs::SetMode offb_set_mode;
-                    offb_set_mode.request.custom_mode = "OFFBOARD"
-                    mavros_msgs::CommandBool arm_cmd;
-                    arm_cmd.request.value = true;
+                if (armed == false){
+                    mavros_msgs::SetMode offb_set_mode;//mode set command object
+                    offb_set_mode.request.custom_mode = "OFFBOARD"//set mode
+                    mavros_msgs::CommandBool arm_cmd;//boolean command object
+                    arm_cmd.request.value = true;//set arm command
+                    //if not in offboard mode and we have asked more than 5 seconds ago, then attempt to set it
                     if( current_state.mode != "OFFBOARD" &&
                         (ros::Time::now() - last_request > ros::Duration(5.0))){
+                        //send arm command and then check it
                         if( set_mode_client.call(offb_set_mode) &&
                             offb_set_mode.response.success){
                             ROS_INFO("Offboard enabled");
                         }
                         last_request = ros::Time::now();
+                    //if we are in offboard mode, try arming
                     } else {
                         if( !current_state.armed &&
                             (ros::Time::now() - last_request > ros::Duration(5.0))){
+                            //try arming and check
                             if( arming_client.call(arm_cmd) &&
                                 arm_cmd.response.success){
                                 ROS_INFO("Vehicle armed");
+                                armed == true;
                             }
                             last_request = ros::Time::now();
                         }
                     }
+                }
+                else {
+                    ROS_INFO("Vehicle prepared");
                 }
                 break;
             case LAUNCH:
